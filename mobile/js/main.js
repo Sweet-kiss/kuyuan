3b9c0d4c0a52caec0f48ddd7cca1fd18c333700f
Sweet-Kiss <1196940127@qq.com>
2017-07-16 22:46:10 +0800

    first commit

---
+++ mobile/js/main.js
@@ -0,0 +1,30 @@
+$("#login .login-btn").click(function() {
+	$(this).fadeOut();
+	$(this).siblings().fadeIn()
+	if ($("#login-panel").is(":hidden")) {
+		$("#login-panel").slideDown("fast")
+	} else {
+		$("#login-panel").slideUp("fast")
+	}
+})
+$("#goods-title").click(function() {
+	if ($(this).find("ul").is(":hidden")) {
+		$(this).css("color", "#FF0000")
+		$(this).find(".goods-center").attr("src", "img/triangle-open.png")
+		$(this).find("ul").slideDown("fast")
+	} else {
+		$(this).css("color", "#000000")
+		$(this).find(".goods-center").attr("src", "img/triangle-close.png")
+		$(this).find("ul").slideUp("fast")
+	}
+})
+$("#goods-title li").click(function(event) {
+	event.stopPropagation()
+})
+var mySwiper = new Swiper('.swiper-container', {
+	autoplay: 5000,
+	autoplayDisableOnInteraction : false,
+	loop : true,
+	prevButton:'.swiper-button-prev',
+	nextButton:'.swiper-button-next',
+})
